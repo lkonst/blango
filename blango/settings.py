cced3cc64d52f21ffa32c2e8043abc5b5485e3ea
Lavrentios Konstantinidis <fisikosnomos@gmail.com>
2024-02-05 18:51:13 +0000

    Finish simplejwt

--- blango/settings.py
+++ blango/settings.py
@@ -16,6 +16,7 @@ from configurations import Configuration
 from configurations import values
 import dj_database_url
 from django.contrib.auth.hashers import Argon2PasswordHasher
+from datetime import timedelta
 
 class Dev(Configuration):
 
@@ -95,6 +96,7 @@ class Dev(Configuration):
         "rest_framework.authentication.BasicAuthentication",
         "rest_framework.authentication.SessionAuthentication",
         "rest_framework.authentication.TokenAuthentication",
+        "rest_framework_simplejwt.authentication.JWTAuthentication"
     ],
 
     "DEFAULT_THROTTLE_CLASSES": [
@@ -285,6 +287,11 @@ class Dev(Configuration):
   ACCOUNT_EMAIL_REQUIRED = True
   ACCOUNT_USERNAME_REQUIRED = False
   ACCOUNT_AUTHENTICATION_METHOD = "email"
+
+  SIMPLE_JWT = {
+    "ACCESS_TOKEN_LIFETIME": timedelta(days=1),
+    "REFRESH_TOKEN_LIFETIME": timedelta(days=7),
+  }
 
 
 class Prod(Dev):
--- blog/api/urls.py
+++ blog/api/urls.py
@@ -13,6 +13,8 @@ from blog.api.views import PostViewSet
 
 from rest_framework.routers import DefaultRouter
 
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+
 router = DefaultRouter()
 router.register("tags", TagViewSet)
 router.register("posts", PostViewSet)
@@ -32,6 +34,8 @@ urlpatterns = [
   # path("posts/<int:pk>", PostDetail.as_view(), name='api_post_detail'),
   path("auth/", include("rest_framework.urls")),
   path("token-auth/", views.obtain_auth_token),
+  path("jwt/", TokenObtainPairView.as_view(), name="jwt_obtain_pair"),
+  path("jwt/refresh/", TokenRefreshView.as_view(), name="jwt_refresh"),
   path("users/<str:email>", UserDetail.as_view(), name="api_user_detail"),
   path("auth/", include("rest_framework.urls")),
   path("token-auth/", views.obtain_auth_token),
